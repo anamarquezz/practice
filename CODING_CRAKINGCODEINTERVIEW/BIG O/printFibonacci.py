4dbc4b0a490aabb8e814fb03f8d41560b30fc0fa
AnaMarquez <anamarquez.edu@hotmail.com>
2022-03-03 22:59:29 -0800

    print fibonnaci

--- CODING_CRAKINGCODEINTERVIEW/BIG O/printFibonacci.py
+++ CODING_CRAKINGCODEINTERVIEW/BIG O/printFibonacci.py
@@ -1,20 +1,19 @@
 def allFib(n):
-    memo = [1]
+    memo = [0] * n
+    print(memo)
     for i in range(n):
         print(i , ":" , fib(i, memo));
 
 def fib(n, memo):
-    print('n', n)  
-    print('memo', memo)  
     if n <= 0:
         return 0
     elif n == 1:
         return 1
-    elif memo[n] >= 0:
+    elif memo[n] > 0:
         return memo[n]
 
     memo[n] = fib(n -1, memo) + fib(n - 2, memo)
-    print(memo)
+    print('memo',memo)
     return memo[n]
 
 n = 10
--- CODING_CRAKINGCODEINTERVIEW/BIG O/printFibonnaciJS.js
+++ CODING_CRAKINGCODEINTERVIEW/BIG O/printFibonnaciJS.js
@@ -7,7 +7,9 @@ function allFib(n) {
 function fib(n, memo) {
     if (n <= 0) return 0;
      else if (n == 1) return 1;
-     else if (memo[n] > 0) return memo[n];
+     if (memo.length > 0)
+        if (memo[n] > 0)
+            return memo[n];
     
      memo[n] = fib(n - 1, memo)+ fib(n - 2, memo);
      return memo[n];
